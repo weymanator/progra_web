e584330890a3da20c68bdd2bc17891248df37793
Andres Weyman <andres@4plus1creative.com>
2020-10-28 08:44:01 -0600

    Express

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-const http = require("http");
-const fs = require("fs");
+const express = require('express');
 const path = require("path");
 
-const server = http.createServer(function requestListener(req, res) {
-    const { url } = req;
+const app = express();
 
-    if (/^\/public\//.test(url)) {
-        console.log(path.join(__dirname, url));
-        const stream = fs.createReadStream(path.join(__dirname, url));
-        stream.pipe(res);
-        return;
-    }
+app.use('/public', express.static('public'));
 
-    if (url === "/" || url === "/home") {
-        const stream = fs.createReadStream("./public/index.html");
-        stream.pipe(res);
-        return;
-    }
-
-    if (url === "/curri") {
-        const stream = fs.createReadStream("./public/curri.html");
-        stream.pipe(res);
-        return;
-    }
-
-    if (url === '/curriJuan') {
-        const stream = fs.createReadStream('./public/curriJuan.html');
-        stream.pipe(res);
-        return;
-    }
-
-    if (url === '/claudio') {
-        const stream = fs.createReadStream('./public/claudio_curri.html');
-        stream.pipe(res);
-        return;
-    }
-
-    res.writeHead(404);
-    res.end();
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-server.listen(7000);
+app.get('/curri', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/curri.html'));
+});
+
+app.get('/curriJuan', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/curriJuan.html'));
+});
+
+app.get('/claudio', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/claudio_curri.html'));
+});
+
+app.listen(7000);
